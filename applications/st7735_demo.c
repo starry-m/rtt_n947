6c3263386e8844d45f73abab8ff6eecfafc48152
ming <2386279494@qq.com>
2024-07-16 01:18:40 +0800

    add lcd clear thread test

--- applications/st7735_demo.c
+++ applications/st7735_demo.c
@@ -97,6 +97,42 @@ int st7735s_init(void) {
     }
      return RT_EOK;
 }
+#define THREAD_PRIORITY         25
+#define THREAD_STACK_SIZE       1024
+#define THREAD_TIMESLICE        5
+static rt_thread_t tid1 = RT_NULL;
+/* 线程1的入口函数 */
+static void thread1_entry(void *parameter)
+{
+    rt_uint32_t count = 0;
+    while (1)
+    {
+        /* 线程1采用低优先级运行，一直打印计数值 */
+        rt_kprintf("thread1 count: %d\n", count ++);
+        rt_thread_mdelay(500);
+        LCD_Clear(RED);
+        rt_thread_mdelay(500);
+        LCD_Clear(GREEN);
+        rt_thread_mdelay(500);
+        LCD_Clear(BLUE);
+    }
+}
 
+
+int st7735s_test(void)
+{
+    /* 创建线程1，名称是thread1，入口是thread1_entry*/
+    tid1 = rt_thread_create("thread1",
+                            thread1_entry, RT_NULL,
+                            THREAD_STACK_SIZE,
+                            THREAD_PRIORITY, THREAD_TIMESLICE);
+    /* 如果获得线程控制块，启动这个线程 */
+    if (tid1 != RT_NULL)
+        rt_thread_startup(tid1);
+
+    return RT_EOK;
+}
+
+INIT_APP_EXPORT(st7735s_test);
 // MSH_CMD_EXPORT(st7735s_init, st7735s_  init);
 
